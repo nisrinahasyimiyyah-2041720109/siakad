af781799022a501ec56b922cb2e3c71167697c65
nisrinahasyimiyyah-2041720109 <hasyimiyyahnisrina@gmail.com>
2022-03-30 20:40:12 +0700

    Membuat CRUD Tabel Mahasiswa

---
+++ app/Models/Mahasiswa.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\Mahasiswa as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Model; //Model Eloquent
+
+class Mahasiswa extends Model //Definisi Model
+{
+    protected $table='mahasiswa'; // Eloquent akan membuat model mahasiswa menyimpan record di tabel mahasiswa
+    protected $primaryKey='nim'; // Memanggil isi DB dengan primaryKey
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'Nim',
+        'Nama',
+        'Kelas',
+        'Jurusan',
+    ];
+};
